907f7301b4db47b1477c546b6e6ba74850c65563
Jonathan Cvinar <joncvinar@gmail.com>
2022-04-19 12:34:04 -0400

    Remove unused files/versions
    Cleaning up

--- authenticate.php
+++ authenticate.php
@@ -3,7 +3,7 @@ session_start();
 // Change this to your connection info.
 $DATABASE_HOST = 'localhost';
 $DATABASE_USER = 'root';
-$DATABASE_PASS = '';
+$DATABASE_PASS = 'root';
 $DATABASE_NAME = 'phplogin';
 // Try and connect using the info above.
 $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
